36aa457ccf1407d033ad39ee502cd5e08e49a107
Korey Andrus <korey86@gmail.com>
2022-03-01 13:21:35 -0600

    MODIFY: Class 5 exercise

--- C5_ClassWork/app.js
+++ C5_ClassWork/app.js
@@ -7,7 +7,6 @@
 const userForm = document.querySelector('.user-form');
 const firstNameEl = document.querySelector('.first-name');
 const lastNameEl  = document.querySelector('.last-name');
-const ageEl       = document.querySelector('.age');
 const email       = document.querySelector('.email');
 const submitBtn   = document.querySelector('.submit-btn');
 const userDataEl  = document.querySelector('.user-info');
@@ -19,11 +18,14 @@ printUserData(users);
 
 function addUserData(e){
     e.preventDefault();
+    if(!userForm.checkValidity()){
+        alert("Full name and email required!");
+        return;
+    };
 
     const user = {
         firstName: firstNameEl.value,
         lastName: lastNameEl.value,
-        age: ageEl.value,
         email: email.value,
     }
 
@@ -35,16 +37,37 @@ function addUserData(e){
 }
 
 function printUserData(users){
-    userDataEl.innerHTML = users.map(user => {
+    userDataEl.innerHTML = users.map((user, index) => {
         return `
-        <ul>
+        <ul data-key="${index}">
             <li>${user.firstName}</li>
             <li>${user.lastName}</li>
-            <li>${user.age}</li>
             <li>${user.email}</li>
+            <span class="close-btn">&times;</span>
+            <span class="edit-btn">&#9998;</span>
         </ul>
     `
     }).join('');
+}
+
+function deleteUserData({target}) {
+    if(!target.classList.contains('close-btn')) return;
+    
+    users.splice(target.parentNode.dataset.key, 1);
+    printUserData(users);
+    storeData(users);
+}
+
+function updateUserData({target}) {
+    if(!target.classList.contains('edit-btn')) return;
+    let user = users.splice(target.parentNode.dataset.key, 1);
+
+    firstNameEl.value = user[0].firstName;
+    lastNameEl.value = user[0].lastName;
+    email.value = user[0].email;
+
+    printUserData(users);
+    storeData(users);
 }
 
 function storeData(data){
@@ -52,3 +75,5 @@ function storeData(data){
 }
 
 submitBtn.addEventListener('click', addUserData);
+userDataEl.addEventListener('click', deleteUserData);
+userDataEl.addEventListener('click', updateUserData);
