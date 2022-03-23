5d310c03c2920a34334e0e136d0825494cf81eb3
Korey Andrus <korey86@gmail.com>
2022-03-22 21:09:58 -0500

    Finish class11 ex4 and 5

--- C11_ClassWork/dynamic-content/src/app/app.module.ts
+++ C11_ClassWork/dynamic-content/src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
