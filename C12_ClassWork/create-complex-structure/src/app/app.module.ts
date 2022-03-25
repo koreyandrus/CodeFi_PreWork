354cbd4c17fbcc55e75b40fa641c6a97913802a2
Korey Andrus <korey86@gmail.com>
2022-03-24 20:09:46 -0500

    Finish class 12 exercises through 5

---
+++ C12_ClassWork/create-complex-structure/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { FriendsComponent } from './friends/friends.component';
+import { FriendsListComponent } from './friends-list/friends-list.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PostsComponent,
+    FriendsComponent,
+    FriendsListComponent,
+    MyProfileComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ C12_ClassWork/demo-boxes/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BoxComponent } from './box/box.component';
+import { BoxesComponent } from './boxes/boxes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BoxComponent,
+    BoxesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
