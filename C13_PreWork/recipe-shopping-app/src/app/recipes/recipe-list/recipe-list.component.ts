bfd3521bde2d9da2e63001d5e7a5fc2b25eedaa8
Korey Andrus <korey86@gmail.com>
2022-03-28 14:46:04 -0500

    Finish C13 prework

--- C13_PreWork/recipe-shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ C13_PreWork/recipe-shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://imgs.search.brave.com/IOJA0HuhjLP5-xxVUOmLh1cVKFV2EF_YZDp7nL6kmAQ/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly8xLmJw/LmJsb2dzcG90LmNv/bS8tZS1HT21ONWN0/MDAvWDFhNXVFVTRI/ckkvQUFBQUFBQUFZ/c0EvbWhfT1BsaHl5/djBSVUdjQ1hUcl9o/aDBXX3NBa3ZmMUh3/Q0xjQkdBc1lIUS9z/MjA0OC9uYWRhbiUy/QnByYXducyUyQmZy/eSUyQjEuSlBH'),
+    new Recipe('A Test Recipe', 'This is a test', 'https://imgs.search.brave.com/IOJA0HuhjLP5-xxVUOmLh1cVKFV2EF_YZDp7nL6kmAQ/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly8xLmJw/LmJsb2dzcG90LmNv/bS8tZS1HT21ONWN0/MDAvWDFhNXVFVTRI/ckkvQUFBQUFBQUFZ/c0EvbWhfT1BsaHl5/djBSVUdjQ1hUcl9o/aDBXX3NBa3ZmMUh3/Q0xjQkdBc1lIUS9z/MjA0OC9uYWRhbiUy/QnByYXducyUyQmZy/eSUyQjEuSlBH'),
+  ];
 
   constructor() { }
 
