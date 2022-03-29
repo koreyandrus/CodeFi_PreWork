bcc1a7005308cdd6aa252f68b30d467460bc948a
Korey Andrus <korey86@gmail.com>
2022-03-28 20:04:31 -0500

    Complete 1-3 exercise Class 13

---
+++ C13_ClassWork/dynamic-button-event-binding/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'dynamic-button-event-binding';
+  isDisabled = true;
+
+
+  toggle() {
+    this.isDisabled = !this.isDisabled;
+  }
+}
