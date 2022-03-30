e01f541481fc18a04574c6384bb00900b0b95217
Korey Andrus <korey86@gmail.com>
2022-03-29 20:43:58 -0500

    Finish C13 exercise 10

---
+++ C13_ClassWork/basic-ngFor-exercise/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'basic-ngFor-exercise';
+  food = '';
+  foods: string[] = [];
+
+  addFood() {
+    this.foods.push(this.food);
+  }
+}
